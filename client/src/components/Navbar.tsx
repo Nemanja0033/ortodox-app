e82dd1d5c2f4e45ac62a190764d1ee1674748e9f
nemanja <antonijevicnemanja68@gmail.com>
2025-01-31 01:13:37 +0100

    FEAT:: navbar menu btn

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,15 +1,22 @@
+import { Menu } from "lucide-react";
+import { useState } from "react"
 
 const Navbar = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className='w-full h-[50px] bg-amber-100 shadow-md items-center flex justify-between'>
-        <div className="flex justify-center ml-20">
+        <div className="flex justify-center md:ml-20 ml-5">
             <b className="text-xl flex items-center">Православни Календар ☦️</b >
         </div>
-        <div className="flex justify-around gap-4 items-center mr-20">
+        <div className="md:flex hidden justify-around gap-4 items-center mr-20">
             <a href="#">Календар за целу годину</a>
             <a href="#">Постови</a>
             <a href="#">О апликацији</a>
         </div>
+
+        <button className="mr-5 cursor-pointer"><Menu /></button>
     </nav>
   )
 }
