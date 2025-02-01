556be91624135742e58804c502e4077d46e559e1
nemanja <antonijevicnemanja68@gmail.com>
2025-02-01 22:50:34 +0100

    PATCH:: configuring routes and pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,17 @@
+import CalendarPage from "./pages/CalendarPage";
 import HomePage from "./pages/HomePage";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router";
+import PostPage from "./pages/PostPage";
 const App = () => {
 
   return (
-    <div>
-         <HomePage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={ <HomePage />} />
+        <Route path="kalendar/:id" element={ <CalendarPage />} />
+        <Route path="post" element={ <PostPage />} />
+      </Routes>
+    </Router>
   )
 }
 
