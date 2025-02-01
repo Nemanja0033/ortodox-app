3cf1a668d5750290e8475a37d19daa5c25d30e98
nemanja <antonijevicnemanja68@gmail.com>
2025-02-02 00:28:50 +0100

    STYLE:: clean code

--- client/src/components/HeroCalendar.tsx
+++ client/src/components/HeroCalendar.tsx
@@ -6,12 +6,12 @@ const HeroCalendar = ({data}: any) => {
 
   
   return (
-    <div className="flex-row w-full h-auto rounded-2xl bg-amber-100 shadow-lg">
-        <nav className="w-full h-12 items-center flex justify-center mb-2">
+    <div className="flex-row w-full h-auto rounded-2xl bg-transparent">
+        <nav className="w-full h-12 items-center bg-amber-100 rounded-full rouden full flex justify-center mb-2">
             <span className="font-bold text-xl">{month}</span>
         </nav>
 
-        <div className="flex-row w-full">
+        <div className="flex-row w-full mt-3 bg-amber-100 rounded-xl">
           {data?.map((d:any, index: number) => (
             <div key={index} className="flex justify-between items-center w-full">
               <div className="flex-row">
