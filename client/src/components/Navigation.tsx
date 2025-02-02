739d9763a4f2469942a75dbba2f8aa1648e02183
nemanja <antonijevicnemanja68@gmail.com>
2025-02-02 21:04:12 +0100

    REFACTOR:: clean code

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -29,23 +29,6 @@ const Navbar = () => {
         )
     }
 
-    // if(isOpenMonths){
-    //     return(
-    //         <div className="flex justify-center z-50">
-    //             <div className="md:w-[80%] w-full absolute rounded-2xl top-0 h-screen flex justify-center items-start text-2xl bg-amber-100">
-    //             <div className="flex-row mt-10">
-    //                 <button className="cursor-pointer hover:text-amber-100" onClick={() => setIsOpenMonths(!isOpenMonths)}><X /></button>
-    //                 {months.map((m, index) => (
-    //                     <div className="grid" key={index}>
-    //                         <Link to={`/kalendar/${m}`} className="text-3xl md:mt-2 mt-0.5 hover:text-amber-100">{m} 2025</Link>
-    //                     </div>
-    //                 ))}
-    //             </div>
-    //         </div>
-    //         </div>
-    //     )
-    // }
-
   return (
     <div className="flex justify-center items-center">
     <nav className={`md:w-[80%] w-[90%] rounded-full h-[70px] mt-5 ${theme === 'light' ? 'bg-amber-100/40 hover:bg-amber-100' : 'bg-black text-white'} backdrop-blur-sms transition-all items-center flex justify-between`}>
