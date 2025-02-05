8a4cfeaa03b01d175bb650e740d185481906fbde
nemanja <antonijevicnemanja68@gmail.com>
2025-02-05 12:10:44 +0100

    FEAT:: calendar filtering mehtods

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -1,41 +1,37 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import TodayCard from "./TodayCard";
 import HeroCalendar from "./HeroCalendar";
-import { useCurrentMonth } from "../context/CurrentMonthContext";
 import { useLanguage } from "../context/LanguageContext";
 import { currentDate } from "../constants/currentDate";
 import { useTheme } from "../context/ThemeContext";
+import { useAnim } from "../hooks/useAnim";
 
 const Hero = () => {
   const [data, setData] = useState<any[]| null>(null);
-  const [calendarData, setCalendarData] = useState<any>();
-  const { month } = useCurrentMonth();
   const { language } = useLanguage();
   const { theme } = useTheme();
-  const [loading, setLoading] = useState(true);
 
   let currentMonthName = new Intl.DateTimeFormat("sr-RS", { month: "long" }).format(new Date());
   let today = new Date();
   let dayOfMonth = today.toLocaleDateString("sr-RS", { day: "numeric" });
 
+  const todayCardRef = useRef<HTMLDivElement | null>(null);
+  useAnim(todayCardRef, 50);
 
   useEffect(() => {
     axios.get('http://localhost:3000/calendar')
       .then((response) => {
         setData(response.data.dani.filter((d: any) => d.datum === currentDate));  // storing data for TodayCard component
-        setCalendarData(response.data.dani.filter((d: any) => d.mesec === month)); // storing data for hero calendar component
-        setLoading(false);
       })
       .catch((error) => {
         console.log(error);
-        setLoading(false);
       });
-  }, [month]);
+  }, []);
 
   return (
     <div className="flex justify-center">
-      <div  className="md:w-[700px] h-96 w-full md:mt-20 mt-12 flex-row place-self-center">
+      <div ref={todayCardRef} className="md:w-[700px] h-96 w-full md:mt-20 mt-12 flex-row place-self-center">
       <h1 className={`text-2xl md:text-start text-center font-bold ${theme === 'light' ? 'text-black' : 'text-white'}`}>{language === 'СР' ? 'Данас је' : 'Danas je'} {dayOfMonth}. {currentMonthName}</h1>
           <div>
             {data?.map((d,index) => (
@@ -50,7 +46,7 @@ const Hero = () => {
 
           <div className="md:mt-24 mt-12 mb-12">
             <h1 className={`${theme === 'light' ? 'text-black' : 'text-white'} font-bold md:text-start text-center text-2xl mb-3`}>{language === 'СР' ? 'Календар ѕа месец дана' : 'Kalendar za mesec dana' }</h1>
-            <HeroCalendar data={calendarData} loading={loading}/>
+            <HeroCalendar/>
           </div>
     </div>
     </div>
--- client/src/components/HeroCalendar.tsx
+++ client/src/components/HeroCalendar.tsx
@@ -1,13 +1,43 @@
+import { useEffect, useState } from "react";
 import { currentDate } from "../constants/currentDate";
 import { useCurrentMonth } from "../context/CurrentMonthContext";
+import { useLanguage } from "../context/LanguageContext";
 import { useTheme } from "../context/ThemeContext";
+import axios from "axios";
 
-const HeroCalendar = ({data, loading}: any) => {
-
+const HeroCalendar = () => {
+  const [data, setData] = useState<any>()
+  const [calendarData, setCalendarData] = useState<any>();
+  const [loading, setLoading] = useState(true);
   const {month} = useCurrentMonth();
   const { theme } = useTheme();
+  const { language } = useLanguage();
 
-  if(loading || data.length == 0){
+  useEffect(() => {
+    axios.get('http://localhost:3000/calendar')
+      .then((response) => {
+        setData(response.data.dani.filter((d:any) => d.mesec === month));
+        setCalendarData(response.data.dani.filter((d: any) => d.mesec === month)); // storing data for hero calendar component
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, [month]);
+
+  const showHolidays = () => {
+    let holidays = data.filter((d: any) => d.crveno_slovo === 'Да');
+    setCalendarData(holidays);
+  };
+
+  const showFastingDays = () => {
+    let fastingDays = data.filter((d: any) => d.post === 'Уље' || d.post === 'Вода');
+    setCalendarData(fastingDays);
+    console.log(fastingDays)
+  }
+
+  if(loading || calendarData.length == 0){
     return(
       <p className="animate-bounce text-2xl text-center">Учитавање података. . . Молимо вас освежите страницу</p>
     )
@@ -15,12 +45,16 @@ const HeroCalendar = ({data, loading}: any) => {
   
   return (
     <div className={`flex-row w-full h-auto rounded-3xl ${theme === 'light' ? ' bg-amber-100/40' : 'bg-black/40'}`}>
-        <nav className={`w-full h-12 items-center ${theme === 'light' ? 'bg-amber-100' : 'bg-black text-white'} rounded-full shadow-md flex justify-center mb-2`}>
+        <nav className={`w-full h-12 items-center ${theme === 'light' ? 'bg-amber-100' : 'bg-black text-white'} rounded-full shadow-md flex justify-around mb-2`}>
             <span className="font-bold text-xl">{month}</span>
+            <div className="flex gap-2">
+              <button onClick={showHolidays} className="bg-amber-300 rounded-full text-black w-auto shadow-md p-1 cursor-pointer hover:opacity-65 transition-all">{language === 'СР' ? 'Црвено Слово/Празник' : 'Crveno Slovo/Praznik'}</button>
+              <button onClick={showFastingDays} className="bg-amber-300 rounded-full text-black w-auto shadow-md p-1 cursor-pointer hover:opacity-65 transition-all">{language === 'СР' ? 'Дани Поста' : 'Dani Posta'}</button>
+            </div>
         </nav>
 
         <div className="flex-row w-full overflow-auto md:h-80 h-screen mt-3 bg-transparent rounded-xl">
-          {data?.map((d:any, index: number) => (
+          {calendarData?.map((d:any, index: number) => (
             <div key={index} className={`flex justify-between h-18 ${d.datum === currentDate ? 'bg-amber-200' : ''} ${theme === 'light' ? 'bg-amber-100/40 hover:bg-amber-100' : 'bg-black/70 hover:bg-black text-white'} transition-all cursor-pointer mt-1 shadow-md rounded-full items-center w-full`}>
               <div className="flex-row ml-3">
                 <span className="md:text-xl text-md">{d.datum.replace('2025-', '')}</span>
