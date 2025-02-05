124d095949265e23117010df8992761bb0477a74
nemanja <antonijevicnemanja68@gmail.com>
2025-02-05 23:35:50 +0100

    FEAT:: save date funcionality

--- client/src/components/HeroCalendar.tsx
+++ client/src/components/HeroCalendar.tsx
@@ -4,18 +4,21 @@ import { useCurrentMonth } from "../context/CurrentMonthContext";
 import { useLanguage } from "../context/LanguageContext";
 import { useTheme } from "../context/ThemeContext";
 import axios from "axios";
-import { RefreshCcw } from "lucide-react";
+import { CalendarHeart, RefreshCcw } from "lucide-react";
 
 const HeroCalendar = () => {
   const [data, setData] = useState<any>()
   const [calendarData, setCalendarData] = useState<any>();
   const [loading, setLoading] = useState(true);
-  const [dropdownValue, setDropdownValue] = useState<string>('');
+  const [savedDate, setSavedDate] = useState<any>();
   const {month} = useCurrentMonth();
   const { theme } = useTheme();
   const { language } = useLanguage();
 
-  console.log(dropdownValue)
+
+  useEffect(() => {
+    setSavedDate(localStorage.getItem('saved'));
+  }, []);
 
   useEffect(() => {
     axios.get('http://localhost:3000/calendar')
@@ -41,11 +44,16 @@ const HeroCalendar = () => {
       setCalendarData(data.filter((d:any) => d.dan === 'Недеља'));
     }
   }
-  
 
   const resetFilters = () => {
     setCalendarData(data);
   }
+
+  const saveDate = (date: string) => {
+    localStorage.setItem('saved', date);
+    setSavedDate(date); 
+  };
+  
 
   if(loading || calendarData.length == 0){
     return(
@@ -59,18 +67,18 @@ const HeroCalendar = () => {
             <span className="font-bold text-xl ml-10">{month}</span>
             <div className="flex md:gap-2 gap-1 mr-10 items-center">
               <label className={`${theme === 'light' ? 'text-black' : 'text-white'} font-bold`} htmlFor="dropdown">{language === 'SR' ? 'Sortiraj' : 'Сортирај'}</label>
-              <select className={`${theme === 'light' ? 'bg-amber-100 border border-amber-200' : 'bg-black border-amber-200 border'} rounded-2xl text-center`} onChange={(e) => handleFilter(e.target.value)} name="dropdown">
+              <select className={`${theme === 'light' ? 'bg-amber-200 border border-amber-300' : 'bg-black border-amber-300 border'} rounded-2xl text-center`} onChange={(e) => handleFilter(e.target.value)} name="dropdown">
                 <option>{language === 'SR' ? 'Crveno Slovo/Praznik' : 'Црвено Слово/Празник'}</option>
                 <option>{language === 'SR' ? 'Dani Posta' : 'Дани Поста'}</option>
                 <option>{language === 'SR' ? 'Nedelja' : 'Недеља'}</option>
               </select>
-              <button onClick={resetFilters} className="bg-amber-200 rounded-full text-black w-auto shadow-md p-1 md:scale-90 scale-75 cursor-pointer hover:opacity-65 transition-all"><RefreshCcw /></button>
+              <button onClick={resetFilters} className="bg-amber-300 rounded-full text-black w-auto shadow-md p-1 md:scale-90 scale-75 cursor-pointer hover:opacity-65 transition-all"><RefreshCcw /></button>
             </div>
         </nav>
 
         <div className="flex-row w-full overflow-auto md:h-80 h-screen mt-3 bg-transparent rounded-xl">
           {calendarData?.map((d:any, index: number) => (
-            <div key={index} className={`flex justify-between md:h-22 h-18  ${d.datum === currentDate ? 'bg-amber-200' : ''} ${theme === 'light' ? 'bg-amber-100/40 hover:bg-amber-100' : 'bg-black/70 hover:bg-black text-white'} transition-all cursor-pointer mt-1 shadow-md rounded-full items-center w-full`}>
+            <div key={index} className={`flex justify-between md:h-22 h-18 ${savedDate === d.datum ? 'bg-red-400' : ''}  ${d.datum === currentDate ? 'bg-amber-200' : ''} ${theme === 'light' ? 'bg-amber-100/40 hover:bg-amber-100' : 'bg-black/70 hover:bg-black text-white'} transition-all cursor-pointer mt-1 shadow-md rounded-full items-center w-full`}>
               <div className="flex-row ml-5">
                 <span className="md:text-xl text-md">{d.datum.replace('2025-', '')}</span>
                 <br />
@@ -78,7 +86,10 @@ const HeroCalendar = () => {
                 <br />
                 <span>{d.dan}</span>
               </div>
-              <h1 className={`${d.crveno_slovo === 'Да' ? 'text-red-500' : ''} md:text-xl font-bold mr-3 text-end`}>{d.praznik}</h1>
+              <div className="flex gap-2 items-center mr-3">
+                <h1 className={`${d.crveno_slovo === 'Да' ? 'text-red-500' : ''} md:text-xl font-bold mr-3 text-end`}>{d.praznik}</h1>
+                <button onClick={() => saveDate(d.datum)} className="cursor-pointer hover:text-amber-300"><CalendarHeart size={22} /></button>
+              </div>
           </div>
           ))}
         </div>
