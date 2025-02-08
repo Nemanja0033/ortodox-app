15a8b9a65e007b79432b4e973a90414d7728e767
nemanja <antonijevicnemanja68@gmail.com>
2025-02-08 18:32:03 +0100

    STYLE:: clean code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,12 @@ import HomePage from "./pages/HomePage";
 import { BrowserRouter as Router, Route, Routes } from "react-router";
 import PostPage from "./pages/PostPage";
 import AboutPage from "./pages/AboutPage";
+import Navbar from "./components/Navigation";
 const App = () => {
 
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={ <HomePage />} />
         <Route path="kalendar/:id" element={ <CalendarPage />} />
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,11 +1,9 @@
 import Hero from "../components/Hero"
-import Navbar from "../components/Navigation"
 
 
 const HomePage = () => {
   return (
     <>
-      <Navbar />
       <Hero />
     </>
   )
