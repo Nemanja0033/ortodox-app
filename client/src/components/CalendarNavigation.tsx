2381e64ba1ccee96d83b744f15ce4fe90f87cb45
nemanja <antonijevicnemanja68@gmail.com>
2025-02-10 23:49:04 +0100

    FEAT:: grid layout for calendar

---
+++ client/src/components/CalendarNavigation.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CalendarNavigation = () => {
+  return (
+    <div>CalendarNavigation</div>
+  )
+}
+
+export default CalendarNavigation
--- client/src/components/SingleDate.tsx
+++ client/src/components/SingleDate.tsx
@@ -1,3 +1,4 @@
+import { currentDate } from "../constants/currentDate";
 import { useTheme } from "../context/ThemeContext"
 import { SingleDateProps } from "../types/SingleDateComponentType"
 
@@ -6,12 +7,12 @@ const SingleDate = ({datum, praznik, post, crveno_slovo, key}: SingleDateProps) 
     const { theme } = useTheme();
 
   return (
-    <div key={key} className={`w-auto h-32 overflow-auto flex-row rounded-xl ${theme === 'light' ? 'bg-amber-100' : 'bg-black text-white'} text-center`}>
-        <h3 className={`${crveno_slovo === 'Да' ? 'text-red-500' : ''}`}>{praznik}</h3>
-        <div className="flex justify-between px-4">
-            <span>{datum}</span>
-            <span>{post}</span>
+    <div key={key} className={`w-auto h-32 overflow-auto flex-row rounded-xl ${datum === currentDate.replace('2025-', '') ? 'bg-amber-200' : ''} ${theme === 'light' ? 'bg-amber-100' : 'bg-black text-white'} text-center`}>
+         <div className="flex justify-between px-4 mb-3">
+            <span className="font-bold">{datum}</span>
+            <span className="font-bold">{post}</span>
         </div>
+        <h3 className={`${crveno_slovo === 'Да' ? 'text-red-500 font-bold' : ''}`}>{praznik}</h3>
     </div>
   )
 }
