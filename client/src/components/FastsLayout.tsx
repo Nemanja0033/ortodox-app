5c431ffd98bab2f41edc8a16439176552b7d570f
nemanja <antonijevicnemanja68@gmail.com>
2025-02-13 15:02:08 +0100

    FEAT:: FastsView layout

--- client/src/components/FastsLayout.tsx
+++ client/src/components/FastsLayout.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useTheme } from "../context/ThemeContext";
 import axios from "axios";
+import FastsView from "./FastsView";
 
 const FastsLayout = () => {
     const { theme } = useTheme();
@@ -14,9 +15,16 @@ const FastsLayout = () => {
     }, []);
 
   return (
-    <main className={`w-full rounded-2xl mt-10 h-full grid grid-cols-1 ${theme === 'light' ? 'bg-amber-100/40' : 'bg-black text-white'}`}>
+    <main className={`w-full rounded-2xl mt-10 h-screen grid grid-cols-1 ${theme === 'light' ? 'bg-amber-100/40' : 'bg-black text-white'}`}>
       <section className="flex-row text-center w-full m-3">
         <h1 className="text-2xl font-bold">{data?.uvod[0].naslov}</h1>
+        <p className="text-xl mt-5">``{data?.uvod[0].tekst} ,,</p>
+      </section>
+
+      <section className="grid grid-cols-1 place-items-center">
+        {data?.postovi.map((p: any, i: any) => (
+          <FastsView key={i} post={p.post} />
+        ))}
       </section>
     </main>
   )
--- client/src/components/FastsView.tsx
+++ client/src/components/FastsView.tsx
@@ -5,8 +5,8 @@ const FastsView = ({post}: {post: string}) => {
     const {theme} = useTheme();
 
   return (
-    <div className={`w-full h-20 flex justify-center items-center rounded-2xl ${theme === 'light' ? 'bg-amber-100/40' : 'bg-black text-white'}`}>
-        <h1 className="font-bold">{post}</h1>
+    <div className={`w-1/2 h-20 flex justify-center items-center rounded-2xl ${theme === 'light' ? 'bg-amber-100/40' : 'bg-black text-white'}`}>
+        <h1 className="font-bold text-xl">{post}</h1>
     </div>
   )
 }
