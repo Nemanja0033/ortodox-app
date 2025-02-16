7ab34866b8d2b4c2b9b26da0d3183325f2029c99
nemanja <antonijevicnemanja68@gmail.com>
2025-02-17 00:53:30 +0100

    FEAT:: ui and doantions links

--- client/src/components/containers/AboutLayout.tsx
+++ client/src/components/containers/AboutLayout.tsx
@@ -1,10 +1,18 @@
 import { useState, useEffect } from "react";
 import { useTheme } from "../../context/ThemeContext";
 import axios from "axios";
+import { useLanguage } from "../../context/LanguageContext";
+import { Link } from "react-router";
+
+type DataProps = {
+	aboutRS: string,
+	aboutRS_cyr: string
+}
 
 const AboutLayout = () => {
-	const [data, setData] = useState();
+	const [data, setData] = useState<DataProps>();
 	const { theme } = useTheme();
+	const { language } = useLanguage();
 
 	useEffect(() => {
 		axios.get('http://localhost:3000/about')
@@ -18,10 +26,15 @@ const AboutLayout = () => {
 	
 	return(
 		<main className={`h-screen flex justify-center text-center`}>
-			<section className={`w-[80%] mt-5 ${theme === 'light' ? 'bg-amber-100/40' : 'bg-black text-white'} flex-ro`}>
-				<h1 className="text-2xl font-bold">O Aplikaciji</h1>
+			<section className={`md:w-[80%] w-full mt-5 ${theme === 'light' ? 'bg-amber-100/40' : 'bg-black text-white'} flex-ro`}>
+				<h1 className="text-2xl font-bold">{language === 'SR' ? "O Aplikaciji" : "О Апликацији"}</h1>
 				<br />
-				<p className="font-bold text-xl">{data?.aboutRS}</p>						
+				<p className="font-bold text-xl">{language === 'SR' ? data?.aboutRS : data?.aboutRS_cyr}</p>						
+				<div className="flex justify-center items-center mt-5">
+					<Link to={'https://buymeacoffee.com/nemanja0033'}>
+						<img className="md:h-46 h-60" src="/donations.png" alt="" />
+					</Link>
+				</div>
 			</section>
 		</main>
 	)
